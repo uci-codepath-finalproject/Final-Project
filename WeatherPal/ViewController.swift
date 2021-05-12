5e7384f3b491d167d24f03da6a2b1fa8744c8ad4
Hao Chiang <haomingchiang@gmail.com>
2021-05-11 19:20:50 -0700

    initial commit

---
+++ WeatherPal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherPal
+//
+//  Created by Hao-Ming Chiang on 5/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
