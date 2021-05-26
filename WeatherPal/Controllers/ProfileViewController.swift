66a3c876bf48922f326003e2afd99eeee8345d4b
Hao Chiang <haomingchiang@gmail.com>
2021-05-25 17:48:01 -0700

    added functionality to logout

--- WeatherPal/Controllers/ProfileViewController.swift
+++ WeatherPal/Controllers/ProfileViewController.swift
@@ -28,7 +28,15 @@ class ProfileViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        delegate.window?.rootViewController = loginViewController
+    }
+    
     /*
     // MARK: - Navigation
 
